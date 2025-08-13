07de7c523737eec94b987b7b9ccfa4a74d6e9628
Aliaksandr Torkhau <aliaksandr@sageinclusion.com>
2025-08-13 21:51:14 +0200

    fix: adjust dimensions and spawn points in Belarus level configuration

--- src/core/const.js
+++ src/core/const.js
@@ -8,10 +8,10 @@ export const levelFolder = `${import.meta.env.BASE_URL}levels`;
 export const spawnPoints = {
   start: { roomName: 'belarus', spawnPoint: { x: 23, y: 53 } },
   toBelarus: { roomName: 'belarus', spawnPoint: { x: 7, y: 35 } },
-  toBelarusFromHome: { roomName: 'belarus', spawnPoint: { x: 28, y: 54 } },
+  toBelarusFromHome: { roomName: 'belarus', spawnPoint: { x: 28, y: 53 } },
   toBelarusFromWork: { roomName: 'belarus', spawnPoint: { x: 40, y: 22 } },
   toBelarusFromCollage: { roomName: 'belarus', spawnPoint: { x: 27, y: 15 } },
-  toBelarusHome: { roomName: 'belarusHome', spawnPoint: { x: 6, y: 11 } },
+  toBelarusHome: { roomName: 'belarusHome', spawnPoint: { x: 6, y: 12 } },
   toBelarusWork: { roomName: 'belarusWork', spawnPoint: { x: 8, y: 11 } },
   toBelarusCollage: { roomName: 'belarusCollage', spawnPoint: { x: 4, y: 9 } },
   toPoland: { roomName: 'poland', spawnPoint: { x: 61, y: 40 } },
